952a8f64ad1405b1c6919a55981287d83a6b092b
Barış Arıburnu <barisariburnu@gmail.com>
2020-04-01 15:04:17 +0300

    Update App.js

--- src/EmployeeList/App.js
+++ src/EmployeeList/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
-import {SafeAreaView, StyleSheet, FlatList} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import data from './data';
 
 export default class App extends Component {
   renderContactItem = ({item, index}) => {
     return (
       <>
-        <View>
-          <Text>{item.name}</Text>
-        </View>
+        <TouchableOpacity style={styles.itemContainer}>
+          <Image style={styles.avatar} source={{uri: item.picture}} />
+          <View style={styles.itemDetailContainer}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text>{item.company}</Text>
+          </View>
+        </TouchableOpacity>
       </>
     );
   };
@@ -16,7 +25,7 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
           <FlatList
             keyExtractor={item => {
               item._id;
@@ -29,6 +38,29 @@ export default class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginHorizontal: 10,
+  },
+  itemDetailContainer: {
+    justifyContent: 'space-around',
+  },
+  name: {
+    fontSize: 16,
+  },
+});
 
 export default App;
