af84975f33116465d2d8f4ca4b2e8ba4b57eeb8a
barisariburnu <barisariburnu@gmail.com>
2020-04-02 16:30:58 +0300

    Update App.js

--- src/LoginPage/App.js
+++ src/LoginPage/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Login from './src/screens/Login';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <>
